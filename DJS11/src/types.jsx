b19df7ffecd901a68d2ddf6b7ec04cbdad2f28c1
Minenhle Ncapai <minipai64@gmail.com>
2024-11-26 00:35:48 +0200

    Add basic structures for api.jsx and types.jsx files

---
+++ DJS11/src/types.jsx
@@ -0,0 +1,20 @@
+// Object structures for application data
+
+export const Show = {
+    id: "",         // Show ID
+    title: "",      // Show title
+    image: "",      // Show image URL
+    genreId: "",    // Genre ID
+};
+
+export const Episode = {
+    id: "",          // Episode ID
+    title: "",       // Episode title
+    audioUrl: "",    // Episode audio URL
+    showId: "",      // Associated show ID
+};
+
+export const Genre = {
+    id: "",          // Genre ID
+    name: "",        // Genre name
+};
